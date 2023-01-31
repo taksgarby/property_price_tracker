f3826e8fe4ea945f2b5f14c1ead4ce01ef70ca7b
Takako Onozuka <onozukatakako@gmail.com>
2023-01-31 11:09:20 +0000

    first commit

---
+++ src/components/navigation/Header.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import "./Header.css";
+
+const Header = () => {
+
+
+ 
+  return (
+    <>
+      <nav className="navbar">
+        <h2>Property Price Tracker</h2>
+      </nav>
+    </>
+  );
+};
+
+export default Header;
---
+++ src/containers/PropertyContainer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../components/navigation/Header";
+
+const PropertyContainer = () => {
+    return ( 
+        <>
+        <Header />
+        <h2>This is the Container</h2>
+        </>
+     );
+}
+ 
+export default PropertyContainer;
